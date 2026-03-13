bbba9955f38576241ce5eca1d49ca36033d1421f
lethanhdatit <thanhdat.it.mmo@gmail.com>
2026-03-13 23:11:45 +0700

    fix css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import './output.css';
 import { AuthProvider } from './hooks/useAuth';
 import ProtectedRoute from './components/ProtectedRoute';
 import AppHeader from './components/AppHeader';
